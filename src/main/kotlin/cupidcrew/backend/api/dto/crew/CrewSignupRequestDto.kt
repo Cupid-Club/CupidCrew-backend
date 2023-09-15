9efa761c7a98bd3d50c5b3fbf9d6812fdf42b0d2
ivy <yookyung.lee@carrotins.com>
2023-09-15 18:53:55 +0900

    test

--- src/main/kotlin/cupidcrew/backend/api/dto/crew/CrewSignupRequestDto.kt
+++ src/main/kotlin/cupidcrew/backend/api/dto/crew/CrewSignupRequestDto.kt
@@ -1,10 +1,13 @@
 package cupidcrew.backend.api.dto.crew
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class CrewSignupRequestDto(
     val name: String,
     val email: String,
     var m_password: String? = "",
     val mutualFriend: String,
     val company: String,
+    @get:JsonProperty("isApproved")
     val isApproved: Boolean?= false,
 )
