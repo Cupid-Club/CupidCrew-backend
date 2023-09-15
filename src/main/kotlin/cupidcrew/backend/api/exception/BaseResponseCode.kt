01cb24b47607cabdc527b36cabfc9aeac0413dd5
ivy <yookyung.lee@carrotins.com>
2023-09-15 16:50:51 +0900

    # conflict 난 거 처리

--- src/main/kotlin/cupidcrew/backend/api/exception/BaseResponseCode.kt
+++ src/main/kotlin/cupidcrew/backend/api/exception/BaseResponseCode.kt
@@ -6,7 +6,8 @@ enum class BaseResponseCode(val code: Int, val message: String) {
     INVALID_PASSWORD(400, "Invalid crew password"),
     DUPLICATE_EMAIL(400, "Duplicate crew by email"),
     DUPLICATE_PHONE_NUMBER(400, "Duplicate candidate by phone number"),
-    NOT_YET_APPROVED_AS_CREW(400, "Not yet approved as crew")
+    NOT_YET_APPROVED_AS_CREW(400, "Not yet approved as crew"),
+    LIMIT_QUALIFICATION_NO_CANDIDATE_REGISTERED(400, "Register at least one candidate to get retrieve qualification")
 
     // ... 다른 코드들 ...
 }
