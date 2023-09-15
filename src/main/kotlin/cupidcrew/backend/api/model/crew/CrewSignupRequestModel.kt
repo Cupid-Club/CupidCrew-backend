7cb05dd543b1f1d5500818d8f587f48a15613849
ivy <yookyung.lee@carrotins.com>
2023-09-15 18:02:54 +0900

    # test

--- src/main/kotlin/cupidcrew/backend/api/model/crew/CrewSignupRequestModel.kt
+++ src/main/kotlin/cupidcrew/backend/api/model/crew/CrewSignupRequestModel.kt
@@ -1,10 +1,10 @@
 package cupidcrew.backend.api.model.crew
 
 data class CrewSignupRequestModel(
+    val isApproved: Int,
     val name: String,
     val email: String,
     var password: String,
     val mutualFriend: String,
     val company: String,
-    val isApproved: Int,
 )
