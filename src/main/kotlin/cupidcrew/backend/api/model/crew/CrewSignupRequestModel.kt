d847878a041cbd5f10921cac8a925dbc40bee993
ivy <yookyung.lee@carrotins.com>
2023-09-15 18:42:23 +0900

    @jsonproperty

--- src/main/kotlin/cupidcrew/backend/api/model/crew/CrewSignupRequestModel.kt
+++ src/main/kotlin/cupidcrew/backend/api/model/crew/CrewSignupRequestModel.kt
@@ -1,10 +1,13 @@
 package cupidcrew.backend.api.model.crew
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class CrewSignupRequestModel(
     val name: String,
     val email: String,
     var password: String,
     val mutualFriend: String,
     val company: String,
+    @JsonProperty("isApproved")
     val isApproved: Boolean?= false,
 )
